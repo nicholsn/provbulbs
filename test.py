7b81790a5f0f44e16ff079f1abe791871556bed0
Nolan Nichols <nolan.nichols@gmail.com>
2012-08-22 01:16:45 -0700

    simple test code

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ config = Config('http://50.112.248.236:8182/graphs/xcede-dm')
 
 interface = Interface(config)
 
-bundle = interface.parse_prov('test.json')
+bundle = interface.parse_prov('workflow_provenance.json')
 
 interface.process_bundle()
 
