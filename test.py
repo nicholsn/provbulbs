4d0eb1c2f2135012f83c75245f04992269a6a7b1
Nolan Nichols <nolan.nichols@gmail.com>
2012-08-23 14:21:06 -0700

    simple example uploading provenance.json to graphdb

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 __author__ = 'nolan'
 
+from bulbs.rexster import Graph, Config
+
 from interface import Interface, Config
 
 config = Config('http://50.112.248.236:8182/graphs/xcede-dm')
+
 
 interface = Interface(config)
 
 bundle = interface.parse_prov('workflow_provenance.json')
 
-interface.process_bundle()
+records = bundle.get_records()
+
+entity = records[0]
+
+response = interface.process_bundle()
 
 
